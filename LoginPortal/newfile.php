5195b6489c0b399cc2bbe40138ffd6079b8dad07
Jonah <Jonah@145.44.61.85>
2015-11-25 10:17:08 +0100

    sa

--- LoginPortal/newfile.php
+++ LoginPortal/newfile.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="stylesheet" href="/StylePortal.css">
+<link rel="stylesheet" href="StylePortal.css">
 <title>Insert title here</title>
 </head>
     <body>
