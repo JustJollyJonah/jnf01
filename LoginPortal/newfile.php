c71019c12cca3b99a72b774111a73f66ae2ed314
Jonah <Jonah@145.44.61.85>
2015-11-25 10:13:17 +0100

    Added

--- LoginPortal/newfile.php
+++ LoginPortal/newfile.php
@@ -1,19 +1,16 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 </head>
-<frameset>
-    <frame>
-    <frame>
-    <noframes>
     <body>
-    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    <div class="banner">
+    	<img src="" alt="Hier komt het logo">
+    	<p>Dynamiek Ateliers Login Portaal</p>
+    </div>
     <?php
 
 	?>
     </body>
-    </noframes>
-</frameset>
 </html>
