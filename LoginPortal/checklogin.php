06e37f78d4c358472920d355edbe2710a578121d
Chris <Chris@145.44.82.83>
2015-12-09 15:18:12 +0100

    .

---
+++ LoginPortal/checklogin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body>
+<?php
+session_start ();
+include ("CustomEncryption.php");
+
+$user = $_POST ['username'];
+$pw = Encrypt($_POST ['password']);
+
+$_SESSION['user'] = $user;
+
+include ("../DatabaseFunctions.php");
+
+$pdo = connectToServer ( "mysql:host=localhost;port=3307", "root", "usbw" );
+
+selectDatabase ( $pdo, "omega" );
+
+LogIn ( $pdo, $user, $pw );
+
+function LogIn($pdo, $user, $pw) {
+	$query = $pdo->prepare( "SELECT COUNT(*) test FROM login WHERE Gebruikersnaam=? AND Wachtwoord=? LIMIT 1" );
+	$query->execute(array($user,$pw));
+	
+	while($row = $query->fetch()){
+		$waarde = $row['test'];
+		if($waarde == 1){
+			header('Location: Ingelogd.php');
+		}else{
+			$_SESSION['falseLogin'] = TRUE;
+			header('Location: login.php');
+			$_SESSION['falseInfo'] = array($user,$pw);
+		}
+	}
+}
+?>
+</body>
+</html>
