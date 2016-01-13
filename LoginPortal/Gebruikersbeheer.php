ea28f559fad2f4a35dc3def8a30a1b7e92b7e35e
Wilbert <Wilbert@145.44.64.222>
2016-01-13 11:20:25 +0100

    fixen gebruiker beheer en toevoegen + encryptie wachtwoord dus inloggen met gebruiker is mogelijk

--- LoginPortal/Gebruikersbeheer.php
+++ LoginPortal/Gebruikersbeheer.php
@@ -1,7 +1,6 @@
 
 <?php
 
- 
 // Start the session, so we can retriev client information.
 session_start();
 $user = $_SESSION['user'];
@@ -10,7 +9,6 @@ $user = $_SESSION['user'];
 include("../DatabaseFunctions.php");
 $db = connectToServer("mysql:host=178.62.201.206;port=3306", "omega", "usbw");
 selectDatabase($db, "omega");
- 
 //if(!isset($_SESSION['user']) || checkUserLevel($db, $user) != 1)
 //{
 //    header('Location: login.php');
@@ -32,19 +30,31 @@ $query = $db->prepare("SELECT * FROM medewerker");
 $query->execute();
  
 $txt_user = '';
- 
+
 while($row = $query->fetch())
 {
+	if($row['Actief'] == 1)
+	{
+		$checked = "Ja";
+	}else{
+		$checked = "Nee";
+	}
+	
     $txt_user .= "
         <tr>
             <td>{$row['Medewerkernummer']}</td>
             <td>{$row['Naam']}</td>
             <td>{$row['Achternaam']}</td>
-            <td><input type=\"checkbox\" name=\"actief\" value=\"{$row['Actief']}\"></td>
+            <td>{$checked}</td>
             <td><a href=\"bewerkMedewerker.php?verwijder={$row['Medewerkernummer']}\">Verwijder</a></td>
             <td><a href=\"bewerkMedewerker.php?wijzig={$row['Medewerkernummer']}\">Wijzigen</a></td>
         </tr>";
 }
+
+
+
+//{$row['Actief']}
+
  
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
@@ -76,7 +86,7 @@ while($row = $query->fetch())
                 <th>Achternaam</th>
                 <th>Actief</th>
             </tr>
-            <a href=Gebruikertoevoegen.php>Gebruiker toevoegen</a>
+            <a class="voegtoe" href="Gebruikertoevoegen.php">Voeg gebruiker toe</a>
             <?php echo $txt_user; ?>
         </table>
     </body>
